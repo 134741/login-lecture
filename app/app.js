ed3702a7261c5f1c98bb355a263984fe55b80775
congs00n <ninxtxn@gmail.com>
2022-07-16 18:02:54 +0900

    프론트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -1,14 +1,22 @@
 "use strict";
 
+// 모듈
 const express = require("express");
 const app = express();
 
+// 라우팅
 const home = require("./src/routes/home");
 
+// 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// 미들웨어
 app.use("/", home); 
+// ${__dirname} => 현재 모듈이 있는 디렉토리 이름을 반환한다.
+// app.js 모듈이 있는 디렉토리는 app 
+// 한 문장으로 정리 => app의 src의 public 폴더를 정적 경로로 추가해 줄 것이다.
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
 
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,5 @@
+'user strict';
+
+// 해당 login.js 모듈은 html 문서와 연결된 js파일이기 때문에 
+// 프론트에서 동작하는 js라고 볼 수 있다.  
+console.log("hello");
